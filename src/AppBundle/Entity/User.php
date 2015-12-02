e873fb6c6d89b5ee79b407547c5e779b5af354e9
Martind64 <Martind_64@hotmail.com>
2015-12-02 14:47:49 +0100

    Added FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
