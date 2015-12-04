365168818be238e3447d8d0d1cabe09ba0c05149
SuperStrandby <strandby@live.dk>
2015-12-04 15:43:51 +0100

    added coupon entity

---
+++ src/AppBundle/Entity/Coupon.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use DateTime;
+
+class Coupon {
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdDatetime;
+
+    /**
+     * @var string
+     */
+    private $wager;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdDatetime
+     *
+     * @param \DateTime $createdDatetime
+     *
+     * @return Coupon
+     */
+    public function setCreatedDatetime($createdDatetime)
+    {
+        $this->createdDatetime = $createdDatetime;
+
+        return $this;
+    }
+
+    /**
+     * Get createdDatetime
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDatetime()
+    {
+        return $this->createdDatetime;
+    }
+
+    /**
+     * Set wager
+     *
+     * @param string $wager
+     *
+     * @return Coupon
+     */
+    public function setWager($wager)
+    {
+        $this->wager = $wager;
+
+        return $this;
+    }
+
+    /**
+     * Get wager
+     *
+     * @return string
+     */
+    public function getWager()
+    {
+        return $this->wager;
+    }
+    /**
+     * @var \AppBundle\Entity\Bet
+     */
+    private $bet;
+
+
+    /**
+     * Set bet
+     *
+     * @param \AppBundle\Entity\Bet $bet
+     *
+     * @return Coupon
+     */
+    public function setBet(\AppBundle\Entity\Bet $bet = null)
+    {
+        $this->bet = $bet;
+
+        return $this;
+    }
+
+    /**
+     * Get bet
+     *
+     * @return \AppBundle\Entity\Bet
+     */
+    public function getBet()
+    {
+        return $this->bet;
+    }
+    /**
+     * @var \AppBundle\Entity\User
+     */
+    private $user;
+
+
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return Coupon
+     */
+    public function setUser(\AppBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
