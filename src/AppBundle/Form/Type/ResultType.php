b639ab3cb42d6ae8b1cb29f8db407c974c7c0119
SuperStrandby <strandby@live.dk>
2015-12-20 04:50:55 +0100

    added startup result view

--- src/AppBundle/Form/Type/ResultType.php
+++ src/AppBundle/Form/Type/ResultType.php
@@ -1,8 +1,12 @@
 <?php
 namespace AppBundle\Form\Type;
 
+use AppBundle\Entity\Bet;
+use AppBundle\Entity\Team;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
 class ResultType extends AbstractType
@@ -10,16 +14,27 @@ class ResultType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('result','choice', [
-                'choices' => ['1' => '1', '2' => '2'],
-                'multiple' => false,
-                'expanded' => false,
-                'label' => 'Winner',
+            ->add('homeTeam', 'entity', [
+                'attr' => ['class' => 'form-control'],
+                'class' => Team::class,
+                'choice_label' => 'name',
+                'empty_value' => 'Select home team',
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.name', 'ASC');
+                }
             ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Bet::class,
+        ));
     }
 
     public function getName()
     {
-        return 'result';
+        return 'bet';
     }
 }
